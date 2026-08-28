b7c47c0a67ff1dc01df25b0e62c7ea8cbe93bc3e
Otavio Trentin <otavio_trentin@hotmail.com>
2026-08-28 11:27:52 -0400

    bump release version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-xero',
-    version='1.0.6',
+    version='1.0.7',
     description='hotglue target for exporting data to Xero API',
     author='hotglue',
     url='https://hotglue.xyz',
